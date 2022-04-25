d5bc429c81df716c49c1a83c3b489bcef6b451e9
Hans Löfgren <mail@1two3code.com>
2022-04-25 17:35:46 +0200

    synth went through omg

--- infra/backstage.py
+++ infra/backstage.py
@@ -1,7 +1,12 @@
-
+import yaml
 from dotenv import dotenv_values
 from aws_cdk import (
-    core, 
+    core,
+    aws_iam as iam,
+    aws_secretsmanager as secrets,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as actions,
+    aws_codebuild as codebuild,
 )
 from collections import OrderedDict
 from .common_resources import CommonResourceStack
@@ -12,10 +17,13 @@ class BackstageStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, props: dict, stages: dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        github_repo = props.get("GITHUB_INFRA_REPO")
+        github_org = props.get("GITHUB_ORG")
 
         crs = CommonResourceStack( self, "infra-common-resources", props)
         pipeline = AppPipelineStack(self, 'backstage-app-pipeline', props, crs)
-
+        
         # we add deploy stages to the pipeline based on stages dict.
         for name,stage in stages.items():
 
@@ -40,67 +48,71 @@ class BackstageStack(core.Stack):
         ### to the backstage app repo to deploy and update
 
         # create the output artifact space for the pipeline
-        source_output = codepipeline.Artifact()
-        build_output = codepipeline.Artifact()
+            source_output = codepipeline.Artifact()
+            build_output = codepipeline.Artifact()
+            github_auth_secret_name = stage.pop("GITHUB_AUTH_SECRET_NAME", None)
+            github_auth_secret = secrets.Secret.from_secret_name_v2(self, "github-auth-secret", github_auth_secret_name)
+            # setup source to be the backstage app source
+            source_action = actions.GitHubSourceAction(
+                oauth_token=github_auth_secret.secret_value,
+                # oauth_token=srs.secret_mapping['AUTH_GITHUB_CLIENT_SECRET'].secret_value,
+                owner=github_org,
+                repo=github_repo,
+                branch='main',
+                action_name="Github-Source",
+                output=source_output
+            )
+            # make codebuild action to use buildspec.yml and feed in env vars from .env
+            # this will build and push new image to ECR repo
+            with open(r'./buildspec.yml') as file:
+                build_spec = yaml.full_load(file)
 
-        # setup source to be the backstage app source
-        source_action = actions.GitHubSourceAction(
-            oauth_token=github_token_secret.secret_value_from_json("secret"),
-            owner=github_org,
-            repo=github_repo,
-            branch='main',
-            action_name="Github-Source",
-            output=source_output
-        )
-        # make codebuild action to use buildspec.yml and feed in env vars from .env
-        # this will build and push new image to ECR repo
+            build_project = codebuild.PipelineProject(
+                self, 
+                "CodebuildProject", 
+                build_spec=codebuild.BuildSpec.from_object(build_spec),
+                #build_spec=codebuild.BuildSpec.from_source_filename('buildspec.yml'),
+                environment=codebuild.BuildEnvironment(build_image=codebuild.LinuxBuildImage.STANDARD_4_0, privileged=True),
+            )
+            policy =  iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryPowerUser")
+            build_project.role.add_managed_policy(policy)
 
-        build_project = codebuild.PipelineProject(
-            self, 
-            "CodebuildProject", 
-            build_spec=codebuild.BuildSpec.from_object(build_spec),
-            #build_spec=codebuild.BuildSpec.from_source_filename('buildspec.yml'),
-            environment=codebuild.BuildEnvironment(build_image=codebuild.LinuxBuildImage.STANDARD_4_0, privileged=True),
-        )
-        policy =  iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryPowerUser")
-        build_project.role.add_managed_policy(policy)
+            # code build action will use docker to build new image and push to ECR
+            # the buildspec.yaml is in the backstage app repo
+            repo_uri = crs.image_repo.repository_uri
 
-        # code build action will use docker to build new image and push to ECR
-        # the buildspec.yaml is in the backstage app repo
-        repo_uri = docker_asset.repository.repository_uri
+            build_action = actions.CodeBuildAction(
+                action_name="Docker-Build",
+                project=build_project,
+                input=source_output,
+                outputs=[build_output],
+                environment_variables={
+                    "REPOSITORY_URI": codebuild.BuildEnvironmentVariable(value=repo_uri),
+                    "AWS_REGION": codebuild.BuildEnvironmentVariable(value=props.get("AWS_REGION")),
+                    "CONTAINER_NAME": codebuild.BuildEnvironmentVariable(value=props.get("CONTAINER_NAME"))
+                },
 
-        build_action = actions.CodeBuildAction(
-            action_name="Docker-Build",
-            project=build_project,
-            input=source_output,
-            outputs=[build_output],
-            environment_variables={
-                "REPOSITORY_URI": codebuild.BuildEnvironmentVariable(value=repo_uri),
-                "AWS_REGION": codebuild.BuildEnvironmentVariable(value=props.get("AWS_REGION")),
-                "CONTAINER_NAME": codebuild.BuildEnvironmentVariable(value=props.get("CONTAINER_NAME"))
-            },
+            )
+            # ECS deploy action will take file made in build stage and update the service with new image
+            deploy_action = actions.EcsDeployAction(
+                service=srs.ecs_stack.service,
+                action_name="ECS-Deploy",
+                input=build_output,
+            )
 
-        )
-        # ECS deploy action will take file made in build stage and update the service with new image
-        deploy_action = actions.EcsDeployAction(
-            service=ecs_stack.service,
-            action_name="ECS-Deploy",
-            input=build_output,
-        )
+            pipeline = codepipeline.Pipeline(self, "fccbackstagepipeline", cross_account_keys=False)
 
-        pipeline = codepipeline.Pipeline(self, "fccbackstagepipeline", cross_account_keys=False)
+            pipeline.add_stage(
+                stage_name="Source",
+                actions=[source_action]
+            )
 
-        pipeline.add_stage(
-            stage_name="Source",
-            actions=[source_action]
-        )
+            pipeline.add_stage(
+                stage_name="Build",
+                actions=[build_action]
+            )
 
-        pipeline.add_stage(
-            stage_name="Build",
-            actions=[build_action]
-        )
-
-        pipeline.add_stage(
-            stage_name="Deploy",
-            actions=[deploy_action]
-        )
+            pipeline.add_stage(
+                stage_name="Deploy",
+                actions=[deploy_action]
+            )
